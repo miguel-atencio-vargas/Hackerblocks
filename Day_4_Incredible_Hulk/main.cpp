e67d287336f4db5d5f92f18f046ca007c9229c9e
skyfall947 <atencio947@gmail.com>
2020-05-03 15:59:29 -0400

    end of day 4

--- Day_4_Incredible_Hulk/main.cpp
+++ Day_4_Incredible_Hulk/main.cpp
@@ -1,1 +1,19 @@
- 
+#include <iostream>
+using namespace std;
+
+
+
+int main(){
+    int N, T;
+    cin>> T;
+    while(T--){
+        int c = 0;
+        cin>>N;
+        while(N>0){
+            N = N & (N - 1);
+            c++;
+        }    
+        cout<<c<<endl;
+    }
+    return 0;
+}
